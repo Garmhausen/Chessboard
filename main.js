61eea865cb284d5efdbacb5a9ccf4053bb628ae2
Garmhausen <robertgarmhausen@gmail.com>
2017-06-13 20:35:12 -0400

    Finish buildBoard() buildPieces() and resetPieces().

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
 var boardWrapper = document.getElementById('boardDisplay');
 var pieces = {};
 var boardMap = {};
-boardMap.xa = '25px';
-boardMap.xb = '105px';
-boardMap.xc = '185px';
-boardMap.xd = '265px';
-boardMap.xe = '345px';
-boardMap.xf = '425px';
-boardMap.xg = '505px';
-boardMap.xh = '585px';
-boardMap.y8 = '25px';
-boardMap.y7 = '105px';
-boardMap.y6 = '185px';
-boardMap.y5 = '265px';
-boardMap.y4 = '345px';
-boardMap.y3 = '425px';
-boardMap.y2 = '505px';
-boardMap.y1 = '585px';
-
 
 function buildBoard() {
   let colArray = ['x', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'x'];
@@ -30,26 +13,20 @@ function buildBoard() {
     for (var j = 0; j < colArray.length; j++) {
       let build = document.createElement("div");
       if (colArray[i] === 'x' && rowArray[j] === 'x') {
-        console.log("build a corner");
         build.setAttribute("class", "corner");
       } else if (j === 0) {
-        console.log("build a left-number of " + rowArray[i]);
         build.setAttribute("class", "left-number");
         build.innerText = rowArray[i];
       } else if (j === 9) {
-        console.log("build a right-number of " + rowArray[i]);
         build.setAttribute("class", "right-number");
         build.innerText = rowArray[i];
       } else if (i === 0) {
-        console.log("build a top-letter of " + colArray[j]);
         build.setAttribute("class", "top-letter");
         build.innerText = colArray[j];
       } else if (i === 9) {
-        console.log("build a bottom-letter of " + colArray[j]);
         build.setAttribute("class", "bottom-letter");
         build.innerText = colArray[j];
       } else {
-        console.log("build a space with the id of " + colArray[j] + rowArray[i]);
         if (white) {
           build.setAttribute("class", "wspace");
           white = false;
@@ -62,22 +39,120 @@ function buildBoard() {
       boardWrapper.appendChild(build);
     }
   }
+
+  // board square locations by x and y coords.  Maybe move into above loop??
+  boardMap.xa = '25px';
+  boardMap.xb = '105px';
+  boardMap.xc = '185px';
+  boardMap.xd = '265px';
+  boardMap.xe = '345px';
+  boardMap.xf = '425px';
+  boardMap.xg = '505px';
+  boardMap.xh = '585px';
+  boardMap.y8 = '25px';
+  boardMap.y7 = '105px';
+  boardMap.y6 = '185px';
+  boardMap.y5 = '265px';
+  boardMap.y4 = '345px';
+  boardMap.y3 = '425px';
+  boardMap.y2 = '505px';
+  boardMap.y1 = '585px';
 }
 
 function resetPieces() {
-  // create each piece as an image and give it a starting location
-  // append each piece to the boardWrapper.  position should be absolute.
-  pieces.bqRook = document.createElement('img');
-  pieces.bqRook.setAttribute('class', 'piece');
-  pieces.bqRook.setAttribute('src', 'images/br.png');
-  pieces.bqRook.style.left = boardMap.a8x;
-  pieces.bqRook.style.top = boardMap.a8y;
-  // let location = boardMap.a8x + ", " + boardMap.a8y;
-  // pieces.bqRook.style.transform = "translate" + location;
 
-  boardWrapper.appendChild(pieces.bqRook);
+  document.getElementById('bqrook').style.transform = "translate(" + boardMap.xa + ", " + boardMap.y8 + ")";
+  document.getElementById('bqknight').style.transform = "translate(" + boardMap.xb + ", " + boardMap.y8 + ")";
+  document.getElementById('bqbishop').style.transform = "translate(" + boardMap.xc + ", " + boardMap.y8 + ")";
+  document.getElementById('bqueen').style.transform = "translate(" + boardMap.xd + ", " + boardMap.y8 + ")";
+  document.getElementById('bking').style.transform = "translate(" + boardMap.xe + ", " + boardMap.y8 + ")";
+  document.getElementById('bkbishop').style.transform = "translate(" + boardMap.xf + ", " + boardMap.y8 + ")";
+  document.getElementById('bkknight').style.transform = "translate(" + boardMap.xg + ", " + boardMap.y8 + ")";
+  document.getElementById('bkrook').style.transform = "translate(" + boardMap.xh + ", " + boardMap.y8 + ")";
+
+  document.getElementById('bapawn').style.transform = "translate(" + boardMap.xa + ", " + boardMap.y7 + ")";
+  document.getElementById('bbpawn').style.transform = "translate(" + boardMap.xb + ", " + boardMap.y7 + ")";
+  document.getElementById('bcpawn').style.transform = "translate(" + boardMap.xc + ", " + boardMap.y7 + ")";
+  document.getElementById('bdpawn').style.transform = "translate(" + boardMap.xd + ", " + boardMap.y7 + ")";
+  document.getElementById('bepawn').style.transform = "translate(" + boardMap.xe + ", " + boardMap.y7 + ")";
+  document.getElementById('bfpawn').style.transform = "translate(" + boardMap.xf + ", " + boardMap.y7 + ")";
+  document.getElementById('bgpawn').style.transform = "translate(" + boardMap.xg + ", " + boardMap.y7 + ")";
+  document.getElementById('bhpawn').style.transform = "translate(" + boardMap.xh + ", " + boardMap.y7 + ")";
+
+
+  document.getElementById('wapawn').style.transform = "translate(" + boardMap.xa + ", " + boardMap.y2 + ")";
+  document.getElementById('wbpawn').style.transform = "translate(" + boardMap.xb + ", " + boardMap.y2 + ")";
+  document.getElementById('wcpawn').style.transform = "translate(" + boardMap.xc + ", " + boardMap.y2 + ")";
+  document.getElementById('wdpawn').style.transform = "translate(" + boardMap.xd + ", " + boardMap.y2 + ")";
+  document.getElementById('wepawn').style.transform = "translate(" + boardMap.xe + ", " + boardMap.y2 + ")";
+  document.getElementById('wfpawn').style.transform = "translate(" + boardMap.xf + ", " + boardMap.y2 + ")";
+  document.getElementById('wgpawn').style.transform = "translate(" + boardMap.xg + ", " + boardMap.y2 + ")";
+  document.getElementById('whpawn').style.transform = "translate(" + boardMap.xh + ", " + boardMap.y2 + ")";
+
+  document.getElementById('wqrook').style.transform = "translate(" + boardMap.xa + ", " + boardMap.y1 + ")";
+  document.getElementById('wqknight').style.transform = "translate(" + boardMap.xb + ", " + boardMap.y1 + ")";
+  document.getElementById('wqbishop').style.transform = "translate(" + boardMap.xc + ", " + boardMap.y1 + ")";
+  document.getElementById('wqueen').style.transform = "translate(" + boardMap.xd + ", " + boardMap.y1 + ")";
+  document.getElementById('wking').style.transform = "translate(" + boardMap.xe + ", " + boardMap.y1 + ")";
+  document.getElementById('wkbishop').style.transform = "translate(" + boardMap.xf + ", " + boardMap.y1 + ")";
+  document.getElementById('wkknight').style.transform = "translate(" + boardMap.xg + ", " + boardMap.y1 + ")";
+  document.getElementById('wkrook').style.transform = "translate(" + boardMap.xh + ", " + boardMap.y1 + ")";
 }
 
-buildBoard();
-buildBoardMap();
-resetPieces();
+function buildPieces() {
+
+  pieces.nameArrayBlack = ['bqrook', 'bqknight', 'bqbishop', 'bqueen', 'bking', 'bkbishop', 'bkknight', 'bkrook', 'bapawn', 'bbpawn', 'bcpawn', 'bdpawn', 'bepawn', 'bfpawn', 'bgpawn', 'bhpawn'];
+  pieces.nameArrayWhite = ['wapawn', 'wbpawn', 'wcpawn', 'wdpawn', 'wepawn', 'wfpawn', 'wgpawn', 'whpawn', 'wqrook', 'wqknight', 'wqbishop', 'wqueen', 'wking', 'wkbishop', 'wkknight', 'wkrook'];
+
+  for (var i = 0; i < pieces.nameArrayBlack.length; i++) {
+    // set attributes
+    let build = document.createElement('img');
+    build.setAttribute('class', 'piece');
+    build.setAttribute('id', pieces.nameArrayBlack[i]);
+
+    // find correct image based on name
+    if (pieces.nameArrayBlack[i] === 'bqrook' || pieces.nameArrayBlack[i] === 'bkrook') {
+      build.setAttribute('src', 'images/br.png');
+    } else if (pieces.nameArrayBlack[i] === 'bqknight' || pieces.nameArrayBlack[i] === 'bkknight') {
+      build.setAttribute('src', 'images/bn.png');
+    } else if (pieces.nameArrayBlack[i] === 'bqbishop' || pieces.nameArrayBlack[i] === 'bkbishop') {
+      build.setAttribute('src', 'images/bb.png');
+    } else if (pieces.nameArrayBlack[i] === 'bqueen') {
+      build.setAttribute('src', 'images/bq.png');
+    } else if (pieces.nameArrayBlack[i] === 'bking') {
+      build.setAttribute('src', 'images/bk.png');
+    } else {
+      build.setAttribute('src', 'images/bp.png');
+    }
+
+    boardWrapper.appendChild(build);
+  }
+
+  for (var i = 0; i < pieces.nameArrayWhite.length; i++) {
+    // set attributes
+    let build = document.createElement('img');
+    build.setAttribute('class', 'piece');
+    build.setAttribute('id', pieces.nameArrayWhite[i]);
+
+    // find correct image based on name
+    if (pieces.nameArrayWhite[i] === 'wqrook' || pieces.nameArrayWhite[i] === 'wkrook') {
+      build.setAttribute('src', 'images/wr.png');
+    } else if (pieces.nameArrayWhite[i] === 'wqknight' || pieces.nameArrayWhite[i] === 'wkknight') {
+      build.setAttribute('src', 'images/wn.png');
+    } else if (pieces.nameArrayWhite[i] === 'wqbishop' || pieces.nameArrayWhite[i] === 'wkbishop') {
+      build.setAttribute('src', 'images/wb.png');
+    } else if (pieces.nameArrayWhite[i] === 'wqueen') {
+      build.setAttribute('src', 'images/wq.png');
+    } else if (pieces.nameArrayWhite[i] === 'wking') {
+      build.setAttribute('src', 'images/wk.png');
+    } else {
+      build.setAttribute('src', 'images/wp.png');
+    }
+
+    boardWrapper.appendChild(build);
+  }
+}
+
+buildBoard();   // build the game board DOM tree...
+buildPieces();  // create the piece elements and append them to the wrapper...
+resetPieces();  // get starting locations for the pieces
